7acc1f9243a728dbd840563a5124ddfe0a6eb171
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-06-11 05:07:43 +0530

    minor

--- quickstart.py
+++ quickstart.py
@@ -18,7 +18,7 @@ session = TwitterPy(use_firefox=True)
 
 with smart_run(session):
     """ Activity flow """
-    # prpare data
+    # prepare data
     target_users_4_copying_followers = ['Allroundraja', 'CFCricket_World', 'D11_Devill10', 'Dream11', 'Dream11Hockey', 'Dream11PKL', 'Dream11Players', 'Dream11Tips2', 'Dream11_Expert', 'Dream11_FCG', 'Dream11Help1', 'Dream11pundit', 'DreamerFantasy7', 'fanfight_app', 'Fantasy_Guruu', 'GuruDream11', 'Haiwaaaan', 'HalaPlayDotCom', 'HARMAN055_', 'IAMSANTIY224', 'india_fantasy', 'IPLExpart', 'I_am_RN_', 'KaushalKiVines', 'LeagueAdda', 'LeagueXofficial', 'MGSDREAM11', 'MyTeam_11', 'Mr360j', 'NarayananXi', 'NBAspecialist21', 'Patelvikesh664', 'PlayerzPot', 'PlayMPL', 'PredictKar', 'Royal_D11Family', 'Royalgoyal23', 'TFGfantasySport', 'WizardlyChamp', 'ballebaazi', 'cricket_dream', 'cricketgeekhere', 'cricpick', 'ddguruji', 'dream11', 'dream11champ321', 'dream11cricinfa', 'dream11sj', 'fantasykfcdream', 'fantasypower11', 'fcnupdates', 'imnandhancric', 'kingaadarsh2', 'lfcsrk', 'lootnook', 'mohitsh229', 'nikhilkomalan', 'peeyushsharmaa', 'shivakanaujia11', 'tips_dream11', 'winner_dream11']
     target_users_4_retweeting = ['ABdeVilliers17', 'henrygayle', 'harbhajan_singh', 'StarSportsIndia', 'YUVSTRONG12', 'yuzi_chahal', 'imkuldeep18', 'klrahul11', 'hardikpandya7', 'Jaspritbumrah93', 'CricketNDTV', 'virendersehwag', 'cricketworldcup', 'circleofcricket', 'ICC',  'BCCI', 'CricInformer', 'sachin_rt', 'bhogleharsha', 'cricbuzz', 'sanjaymanjrekar', 'SGanguly99', 'anilkumble1074']
 
